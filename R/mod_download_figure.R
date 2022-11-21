400d84a512ebe6dce6a4eb74d09e6844f940b43c
$GITHUB_ACTOR <71563929+espors@users.noreply.github.com>
2022-11-21 11:22:53 -0600

    updates

--- R/mod_download_figure.R
+++ R/mod_download_figure.R
@@ -1,68 +1,82 @@
 #' download_figure UI Function
 #'
-#' @description The UI function of the {shiny} module that works with
-#' \code{\link{mod_download_figure_server}()} to automatically download figures
-#'  from within a {shiny} application. The UI function provides a "Download"
-#'  button that results in a pop-up where users can enter plot height and width
-#'  and options to download a plot as PDF, PNG, or SVG. This work for both
-#'  {ggplot2} graphics and base R plots. Base R plots must be saved in an object
-#'  with \code{\link{recordPlot}()}.
+#' @description The UI function of the {shiny} module that works with 
+#' mod_download_figure_server() to automatically download figures from within a 
+#' {shiny} application. The UI function provides a "Download" button that 
+#' results in a pop-up where users can enter plot height and width and options 
+#' to download a plot as PDF, PNG, or SVG. This work for both {ggplot2} graphics 
+#' and base R plots. Base R plots must be saved in an object with recordPlot. 
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
-#' @param label Label for download button. Default is "Download Plot".
-#' @return UI elements for download button.
+#'
+#' @return UI elements for download button. 
 #'
 #' @export
-#'
-#' @importFrom shiny NS tagList
-mod_download_figure_ui <- function(id, label = "Download Plot") {
+#' 
+#' @seealso \code{\link{mod_download_figure_server}}
+#' 
+#' @importFrom shiny NS tagList 
+mod_download_figure_ui <- function(id){
   ns <- NS(id)
-  tagList(
-    actionButton(
-      inputId = ns("download_popup"),
-      label = label
-    ),
-    tippy::tippy_this(
-      ns("download_popup"),
-      "Click to download plot in preferred format and size."
-    )
-  )
+  
+  uiOutput(ns("download"))
 }
-
-#' download_figure Server Function
-#'
-#' @description The server function of the {shiny} module that works with
-#'  \code{\link{mod_download_figure_ui}()} to automatically download figures from
-#'  within a {shiny} application. The server function handles the UI for the
-#'  pop-up and code for downloading the plot as PDF, PNG, or SVG.  This works
-#'  for both ggplot2 graphics and base R plots. Base R plots must be save in an
-#'  object with \code{\link{recordPlot}()}.
+    
+#' download_figure Server Functions
+#' 
+#' @description The server function of the {shiny} module that works with 
+#' mod_download_figure_ui() to automatically download figures from within a 
+#' {shiny} application. The server function handles the UI for the pop-up and 
+#' code for downloading the plot as PDF, PNG, or SVG.  This works for both 
+#' ggplot2 graphics and base R plots. Base R plots must be save in an object 
+#' with recordPlot. 
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
-#' @param filename String designating he name of file that plot should be saved
-#'  as
-#' @param figure Object containg the plot to be downloaded. Should be wrapped in
-#'  a reactive function
-#' @param width Width (inches) the plot should be saved as.
-#'  Default is 8 in and must be a value between 2 and 30.
-#' @param height Height (inches) the plot should be saved as.
-#'  Default is 6 in and must be a value between 2 and 30.
+#' @param filename The name of file that plot should be saved as 
+#' @param figure The plot to be downloaded. Should be wrapped in a reactive 
+#' function 
+#' @param width Width (inches) the plot should be saved as. 
+#' Default is 8 in and must be a value between 2 and 30. 
+#' @param height Height (inches) the plot should be saved as. 
+#' Default is 6 in and must be a value between 2 and 30. 
+#' @param label Character string denoting the label for the download button. 
+#' Default is "Download plot"
+#' 
+#' @return {shiny} server function for download pop-up modal
 #'
 #' @export
-#'
-mod_download_figure_server <- function(id,
-                                       filename,
-                                       figure,
-                                       width = 8,
-                                       height = 6) {
+#' 
+mod_download_figure_server <- function(
+    id, 
+    filename, 
+    figure, 
+    width = 8, 
+    height = 6, 
+    label = "Download Plot"
+) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-
-    # set max and min sizes
-    min_size <- 2
-    max_size <- 30
-
-    # Check width
+    
+    output$download <- renderUI({
+      req(figure())
+      tagList(
+        actionButton(
+          inputId = ns("download_popup"), 
+          icon = icon("download"), 
+          label = label
+        ),
+        tippy::tippy_this(
+          ns("download_popup"), 
+          "Click to download plot in preferred format and size."
+        )
+      )
+    })
+    
+    
+    min_size <- 2 # min for width or height
+    max_size <- 30 # max for width or height
+    
+    #Check width
     figure_width <- reactive({
       if (is.numeric(input$width)) {
         # cutoff the input value to a range as shiny does not enforce
@@ -72,8 +86,8 @@ mod_download_figure_server <- function(id,
         return(width)
       }
     })
-
-    # Check height
+    
+    #Check height
     figure_height <- reactive({
       if (is.numeric(input$width)) {
         # cutoff the input value to a range as shiny does not enforce
@@ -83,31 +97,32 @@ mod_download_figure_server <- function(id,
         return(height)
       }
     })
-
-
+    
+   
     observeEvent(
       input$download_popup,
       {
         showModal(
           modalDialog(
-            numericInput(
+            numericInput(               #Figure width
               inputId = ns("width"),
               label = "Width (in)",
               value = width,
               min = min_size,
               max = max_size
             ),
-            numericInput(
+            numericInput(               #Figure Height
               inputId = ns("height"),
               label = "Height (in)",
               value = height,
               min = min_size,
               max = max_size
             ),
-            h5("The plot will be rendered differently depending on size.
+            h5("The plot will be rendered differently depending on size. 
             When the dimensions are too small, error or blank plot
-               will be generated."),
-            downloadButton(
+               will be generated."
+            ),
+            downloadButton(             #buttons
               outputId = ns("dl_pdf"),
               label = "PDF"
             ),
@@ -119,16 +134,17 @@ mod_download_figure_server <- function(id,
               outputId = ns("dl_svg"),
               label = "SVG"
             ),
-            size = "s"
+            size = "s" 
           )
         )
       }
     )
-
-    # Download PDF ----
+    
+    # Download PDF
     output$dl_pdf <- downloadHandler(
       filename = paste0(filename, ".pdf"),
       content = function(file) {
+        
         on.exit(removeModal())
         pdf(
           file,
@@ -139,11 +155,12 @@ mod_download_figure_server <- function(id,
         dev.off()
       }
     )
-
-    # Download PNG ----
+    
+    # Download PNG
     output$dl_png <- downloadHandler(
       filename = paste0(filename, ".png"),
       content = function(file) {
+        
         on.exit(removeModal())
         png(
           file,
@@ -156,11 +173,12 @@ mod_download_figure_server <- function(id,
         dev.off()
       }
     )
-
-    # Download SVG ---
+    
+    #download SVG
     output$dl_svg <- downloadHandler(
       filename = paste0(filename, ".svg"),
       content = function(file) {
+        
         on.exit(removeModal())
         svg(
           file,
@@ -173,3 +191,9 @@ mod_download_figure_server <- function(id,
     )
   })
 }
+    
+## To be copied in the UI
+# mod_download_figure_ui("download_figure_ui_1")
+    
+## To be copied in the server
+# mod_download_figure_server("download_figure_ui_1")
